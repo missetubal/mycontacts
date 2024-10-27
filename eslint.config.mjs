4d9ebdc62267abc3a00c06c26c13679d494747a8
Michelle Setúbal <76619995+missetubal@users.noreply.github.com>
2024-10-27 15:03:36 -0300

    🎉Initial config files

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+];
